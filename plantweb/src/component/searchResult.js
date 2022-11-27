04540132c2352610c2c36fc5546b0293df2fe2ac
WZNT-KimHyeJin <pcrs5278@naver.com>
2022-11-28 03:02:03 +0900

    [FEAT] page/showDetails API connect

--- plantweb/src/component/searchResult.js
+++ plantweb/src/component/searchResult.js
@@ -1,12 +1,23 @@
 import "./searchResult.css"
+import {useNavigate} from 'react-router-dom';
 
 
 export const SearchResult = (props) => {
+    const navigate = useNavigate();
+
+    const moveTodetailPage=()=>{
+        navigate("/details",{
+            state: {
+                plantID : props.plantID
+            }
+        });
+    }
+
     return (
         <div>
             <li>
                 <table id="result_table">
-                    <tr>
+                    <tr onClick={moveTodetailPage}>
                         <th><img src={props.plantImgUrl}></img></th>
                         <td>{props.plantNameKR} / <i>{props.plantNameEN}</i></td>
                     </tr>
--- plantweb/src/page/showDetails/showDetails.js
+++ plantweb/src/page/showDetails/showDetails.js
@@ -1,18 +1,43 @@
 import Header from "../../component/header";
+import axios from "axios";
+
+import { useNavigate,useLocation } from 'react-router-dom';
 import "./showDetails.css"
+import { useEffect, useState } from "react";
 
 export const ShowDetails=()=>{
+
+    const [detailData, setDetailData]=useState([]);
+    const [isLoaded, setIsLoaded]=useState(false);
+
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    useEffect(()=>{
+        axios.get("localhose:8080",{
+            plantId : location.state.plantId
+        }).then(v=>{
+            
+            setDetailData(v.data);
+            setIsLoaded(true);
+        },
+        e=>{
+            alert("서버 장애");
+        })
+    },[isLoaded])
+
+
+
     return (
         <div>
             <Header></Header>
+
             <div id="details_div">
                 <div>
-                    <div>
-                        <span>사진</span>
-                    </div>
+                    <div><span>사진</span></div>
                     <div><hr></hr></div>
                     <div>
-                        <img></img>
+                        <img src={detailData.plantImgUrl}></img>
                     </div>
                 </div>
 
@@ -26,150 +51,93 @@ export const ShowDetails=()=>{
 
                         <table id="details_table">
                             <tbody>
-                            <tr>
-                                <th><strong>식물 한글명</strong></th>
-                                <td> plantNameKR</td>
-                            </tr>
+                                <tr>
+                                    <th><strong>식물 한글명</strong></th>
+                                    <td> {detailData.plantNameKR}</td>
+                                </tr>
 
-                            <tr>
-                                <th>
-                                    <strong>식물 영어명</strong>
-                                </th>
-                                <td>
-                                    plantNameEN
-                                </td>
-                            </tr>
+                                <tr>
+                                    <th> <strong>식물 영어명</strong></th>
+                                    <td>{detailData.plantNameEN} </td>
+                                </tr>
 
-                            <tr>
-                                <th>
-                                    <strong>식물 설명</strong>
-                                </th>
-                                <td>
-                                    plantDescription
-                                </td>
-                            </tr>
+                                <tr>
+                                    <th> <strong>식물 설명</strong></th>
+                                    <td> {detailData.plantDescription} </td>
+                                </tr>
 
-                            <tr>
-                                <th>
-                                    <strong>원산지</strong>
-                                </th>
-                                <td>
-                                    countryOfOrigin
-                                </td>
-                            </tr>
+                                <tr>
+                                    <th> <strong>원산지</strong></th>
+                                    <td> {detailData.countryOfOrigin}</td>
+                                </tr>
 
-                            <tr>
-                                <th>
-                                    <strong>꽃</strong>
-                                </th>
-                                <td>
-                                    flowerExist
-                                </td>
-                            </tr>
+                                <tr>
+                                    <th><strong>꽃</strong></th>
+                                    <td> {detailData.flowerExist}</td>
+                                </tr>
 
-                            <tr>
-                                <th>
-                                    <strong>식물명</strong>
-                                </th>
-                                <td>
-                                    plantName
-                                </td>
-                            </tr>
+                                <tr>
+                                    <th><strong>식물명</strong></th>
+                                    <td> {detailData.plantName}</td>
+                                </tr>
 
-                            <tr>
-                                <th>
-                                    <strong>식물 특징</strong>
-                                </th>
-                                <td>
-                                    growthPattern
-                                </td>
-                            </tr>
+                                <tr>
+                                    <th><strong>식물 특징</strong></th>
+                                    <td> {detailData.growthPattern}</td>
+                                </tr>
 
-                            <tr>
-                                <th>
-                                    <strong>생장 속도</strong>
-                                </th>
-                                <td>
-                                    growthRate
-                                </td>
-                            </tr>
+                                <tr>
+                                    <th><strong>생장 속도</strong></th>
+                                    <td>{detailData.growthRate}</td>
+                                </tr>
 
-                            <tr>
-                                <th>
-                                    <strong>광</strong>
-                                </th>
-                                <td>
-                                    lightAmount
-                                </td>
-                            </tr>
+                                <tr>
+                                    <th><strong>광</strong></th>
+                                    <td> {detailData.lightAmount}</td>
+                                </tr>
 
-                            <tr>
-                                <th>
-                                    <strong>물주기</strong>
-                                </th>
-                                <td>
-                                    watering
-                                </td>
-                            </tr>
+                                <tr>
+                                    <th><strong>물주기</strong></th>
+                                    <td> {detailData.watering}</td>
+                                </tr>
 
-                            <tr>
-                                <th>
-                                    <strong>병충해</strong>
-                                </th>
-                                <td>
-                                    demangeOfBugs
-                                </td>
-                            </tr>
+                                <tr>
+                                    <th><strong>병충해</strong> </th>
+                                    <td> {detailData.demangeOfBugs}</td>
+                                </tr>
 
-                            <tr>
-                                <th>
-                                    <strong>관리 수준</strong>
-                                </th>
-                                <td>
-                                    manageLevel
-                                </td>
-                            </tr>
+                                <tr>
+                                    <th><strong>관리 수준</strong></th>
+                                    <td>{detailData.manageLevel}</td>
+                                </tr>
 
-                            <tr>
-                                <th>
-                                    <strong>관리 요구도</strong>
-                                </th>
-                                <td>
-                                    manageDemand
-                                </td>
-                            </tr>
+                                <tr>
+                                    <th><strong>관리 요구도</strong></th>
+                                    <td> {detailData.manageDemand} </td>
+                                </tr>
 
-                            <tr>
-                                <th>
-                                    <strong>비료</strong>
-                                </th>
-                                <td>
-                                    fertilizer
-                                </td>
-                            </tr>
+                                <tr>
+                                    <th><strong>비료</strong></th>
+                                    <td>{detailData.fertilizer}</td>
+                                </tr>
 
-                            <tr>
-                                <th>
-                                    <strong>배치 장소</strong>
-                                </th>
-                                <td>
-                                    placeOfDeployment
-                                </td>
-                            </tr>
+                                <tr>
+                                    <th><strong>배치 장소</strong></th>
+                                    <td>{detailData.placeOfDeployment}</td>
+                                </tr>
 
-                            <tr>
-                                <th>
-                                    <strong>재배 팁</strong>
-                                </th>
-                                <td>
-                                    plantingTip
-                                </td>
-                            </tr>
+                                <tr>
+                                    <th> <strong>재배 팁</strong></th>
+                                    <td> {detailData.plantingTip}</td>
+                                </tr>
                             </tbody>
                         </table>
                     </div>
 
                 </div>
+            </div>
+            <div className="buttonName">
+                <button>관심 등록<span>♡</span></button>
             </div>
         </div>
     );
